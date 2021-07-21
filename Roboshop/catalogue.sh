2080e3c6bdd53854e9fc15c19dfcb15d565b838e
Surya <suryapatneedi0505@gmail.com>
2021-07-21 20:06:24 +0530

    seeting up monogodb4

--- Roboshop/catalogue.sh
+++ Roboshop/catalogue.sh
@@ -6,8 +6,16 @@ yum  install nodejs make gcc-c++ -y &>>LOG
 STAT_CHECK $?
 
 print "Adding roboshop user\t"
+id roboshop &>>
+if [$? -ne 0]; then
 useradd roboshop &>>LOG
+fi
 STAT_CHECK $?
 
+print "Download application code"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>LOG
+STAT_CHECK $?
 
+Print "extract download code"
+cd /home/roboshop &>>LOG && unzip /tmp/catalogue.zip &>>LOG && mv catalogue-main catalogue &>>LOG && cd /home/roboshop/catalogue &>>LOG && npm install >>LOG
 
