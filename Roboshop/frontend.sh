e2edc2da0f404788c7db583888f7dc9cb2795686
Surya <suryapatneedi0505@gmail.com>
2021-07-21 14:11:16 +0530

    if condition2

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -6,7 +6,7 @@ rm -f $LOG
 echo -e "Installing Nginx\t..."
 yum install nginx -y  >>$LOG
 if [$? -eq 0 ]; then
-  echo -n -e "\e[32mdone\e[0m"
+  echo -n -e "\e[32m done\e[0m"
 else
   echo fail
 fi
@@ -15,14 +15,14 @@ echo "Enabling Nginx\t..."
 systemctl enable nginx
 
 if [$? -eq 0]; then
-  echo -e -n "\e[21mdone\e[0m"
+  echo -e -n "\e[32m done\e[0m"
 else
     echo fail
     fi
 echo "Starting Nginx"
 systemctl start nginx
 if [$? -eq 0]; then
-  echo -e -n "\e[32mdone\e[0m"
+  echo -e -n "\e[32m done\e[0m"
 else
      echo fail
 fi
