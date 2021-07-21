7ca956d066778d437f0fc6fd046338459d014ecb
Surya <suryapatneedi0505@gmail.com>
2021-07-21 16:37:37 +0530

    common file1

--- Roboshop/common.sh
+++ Roboshop/common.sh
@@ -3,13 +3,15 @@
 LOG=/tmp/roboshop.log
 rm -f $LOG
 
-STAT_CHECK(){
-  if [$1 -eq 0]; then
+
+STAT_CHECK() {
+  if [ $1 -eq 0 ]; then
     echo -e "\e[32m done\e[0m"
-    else
-      echo -e "\e[32m  fail \e[0m"
-      exit 1
-      fi
+  else
+    echo -e "\e[31m fail\e[0m"
+    echo -e "\e[33m Check the log file for more details, Log File - $LOG\e[0m"
+    exit 1
+  fi
 }
 
 print (){
