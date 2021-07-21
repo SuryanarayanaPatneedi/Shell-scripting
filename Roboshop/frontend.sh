27c67a06bca8eda67d910d03994e663cf7a2a7a4
Surya <suryapatneedi0505@gmail.com>
2021-07-21 12:58:58 +0530

    mod9

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -5,9 +5,10 @@ rm -f $LOG
 
 echo -e "Installing Nginx\t...\t\e[32mDONE\e[0m"
 yum install nginx -y  >>$LOG
-
+echo $?
 echo "Enabling Nginx"
 systemctl enable nginx
-
+echo $?
 echo "Starting Nginx"
 systemctl start nginx
+echo $?
