c39f4288a9067e0c153161f408560659b5ab66e7
Surya <suryapatneedi0505@gmail.com>
2021-07-21 19:20:36 +0530

    seeting up monogodb3

--- Roboshop/mongodb.sh
+++ Roboshop/mongodb.sh
@@ -13,6 +13,7 @@ STAT_CHECK $?
 print "Install mongodb\t"
 yum install -y mongodb-org &>>LOG
 STAT_CHECK $?
+#asd
 
 print "updating Mongodb listen  Address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
