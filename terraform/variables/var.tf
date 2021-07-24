00d7f6c191c22917b527b092ffccc66306084e83
Surya <suryapatneedi0505@gmail.com>
2021-07-24 10:21:06 +0530

    var declare

---
+++ terraform/variables/var.tf
@@ -0,0 +1,14 @@
+variable "sample" {
+  default = "Suryanarayana"
+}
+
+output "sample" {
+  value = var.sample
+}
+variable "sample2" {
+  default = "Narayana"
+}
+
+output "sample" {
+  value = var.sample2
+}
