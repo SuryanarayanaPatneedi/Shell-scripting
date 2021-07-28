f48b7c42c46457ba805e5f994b31fbcac448388f
Surya <suryapatneedi0505@gmail.com>
2021-07-28 16:46:44 +0530

    variable

--- terraform/variables/var.tf
+++ terraform/variables/var.tf
@@ -1,78 +1,7 @@
 variable "sample" {
-  default = "Suryanarayana"
+  default = "Hellow world"
 }
 
 output "sample" {
   value = var.sample
 }
-variable "sample2" {
-  default = "Narayana"
-}
-
-output "sample2" {
-  value = var.sample2
-}
-
-variable "number" {
-  default = 100
-}
-
-output "printingnumber" {
-  value = var.number
-}
-variable "boolean" {
-  default = true
-}
-
-output "declareboolean" {
-  value =var.boolean
-}
-
-
-variable "list" {
-  default = [
-    "Hellow",
-    100,
-    true
-  ]
-}
-
-output "listdeclare" {
-  value = var.list
-}
-
-variable "" {}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
