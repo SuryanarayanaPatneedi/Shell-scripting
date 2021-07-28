36138edff363631fcdd32c2f92babc8eece5188b
Surya <suryapatneedi0505@gmail.com>
2021-07-28 16:57:04 +0530

    variable

--- terraform/variables/var.tf
+++ terraform/variables/var.tf
@@ -11,3 +11,28 @@ variable "sample2"{}
 output "sample2" {
   value = var.sample2
 }
+
+output "sample2" {
+  value = "Hellow"
+}
+
+variable "sample3" {
+  value = 100
+}
+
+variable "sample4" {
+  value= true
+}
+
+
+output "sample2" {
+  value = var.sample2
+}
+
+output "sample3" {
+  value = var.sample3
+}
+
+output "sample4" {
+  value = var.sample4
+}
