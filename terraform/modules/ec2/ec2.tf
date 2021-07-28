56dfaf4071c1d2f7c9e242a06c0f0febf049b88d
Surya <suryapatneedi0505@gmail.com>
2021-07-29 01:37:29 +0530

    variable

--- terraform/modules/ec2/ec2.tf
+++ terraform/modules/ec2/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "sample" {
   ami           = "ami-074df373d6bafa625"
-  vpc_security_group_ids = var.sg_id
+  vpc_security_group_ids = [var.sg_id]
   instance_type = "t3.micro"
 
   tags = {
