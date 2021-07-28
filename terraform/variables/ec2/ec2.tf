d182bdadb9e2fcd4a2aef57033b05e15c18101c5
Surya <suryapatneedi0505@gmail.com>
2021-07-29 00:02:46 +0530

    variable

--- terraform/variables/ec2/ec2.tf
+++ terraform/variables/ec2/ec2.tf
@@ -1,43 +1,28 @@
-resource "aws_instance" "sample" {
-  ami                   = "ami-074df373d6bafa625"
-  instance_type         = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-
-  tags                  = {
-    Name                = "Sample"
-  }
-}
-
 resource "aws_security_group" "allow_ssh" {
-  name                  = "allow_ssh"
-  description           = "allow_ssh"
+  name               = "allow_ssh"
+  description        = "Allow ssh"
 
   ingress {
-    description         = "SSH"
-    from_port           = 22
-    to_port             = 22
-    protocol            = "tcp"
-    cidr_blocks         = ["0.0.0.0/0"]
-  }
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0]
+      }
 
-  egress {
-    from_port           = 0
-    to_port             = 0
-    protocol            = "-1"
-    cidr_blocks         = ["0.0.0.0/0"]
-  }
+      egress {
+        from_port    = 0
+        to_port      = 0
+        protocol     = "-1"
+        cidr_blocks  = ["0.0.0.0/0"]
 
-  tags                  = {
-    Name                = "allow_ssh"
-  }
-}
+      }
 
-output "ec2-attributes" {
-  value = aws_instance.sample
-}
+      tags           = {
+        Name         = "allow_ssh"
+      }
+      }
 
-provider "aws" {
-  region = "us-east-1"
-}
-
-#something
+      output"sg-attribute"{
+        value=aws_security_group.allow_ssh
+      }
