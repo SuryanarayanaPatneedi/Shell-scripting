875a25799805a6947f0da2160e35032851540fc8
Surya <suryapatneedi0505@gmail.com>
2021-07-29 00:11:10 +0530

    variable

--- terraform/variables/ec2/ec2.tf
+++ terraform/variables/ec2/ec2.tf
@@ -1,28 +1,32 @@
 resource "aws_security_group" "allow_ssh" {
-  name               = "allow_ssh"
-  description        = "Allow ssh"
+  name        = "allow_ssh"
+  description = "Allow TLS inbound traffic"
+
 
   ingress {
     description      = "ssh"
     from_port        = 22
     to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0]
-      }
+    protocol         = "ssh"
+    cidr_blocks      = [0.0.0.0/0]
+  }
 
-      egress {
-        from_port    = 0
-        to_port      = 0
-        protocol     = "-1"
-        cidr_blocks  = ["0.0.0.0/0"]
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
-      }
+  tags = {
+    Name = "allow_ssh"
+  }
+}
 
-      tags           = {
-        Name         = "allow_ssh"
-      }
-      }
+output "sg_attibutes" {
+  value = aws_security_group.allow_ssh
+}
 
-      output"sg-attribute"{
-        value=aws_security_group.allow_ssh
-      }
+provider "aws" {
+  region = "us-est-1"
+}
