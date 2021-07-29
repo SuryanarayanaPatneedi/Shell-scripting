3893f31400ec0bb5cafe5e2ecbb1b452475fc951
Surya <suryapatneedi0505@gmail.com>
2021-07-29 22:05:07 +0530

    variable

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -2,40 +2,35 @@
 
 source common.sh
 
-print "Installing Nginx"
-yum install nginx -y  &>>$LOG
+PRINT "Installing Nginx\t"
+yum install nginx -y &>>$LOG
 STAT_CHECK $?
 
-print "Enabling Nginx\t"
-systemctl enable nginx &>>$LOG
-STAT_CHECK $?
-
-print "Starting nginx\t"
-systemctl start nginx &>>$LOG
-STAT_CHECK $?
-
-print "Downloading Frontend "
+PRINT "Download Frontend\t"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
 STAT_CHECK $?
 
-print "Removing htDoc\t"
-cd /usr/share/nginx/html  &>>$LOG &&  rm -rf * &>>$LOG
+
+PRINT "Remove Old HtDocs\t"
+cd /usr/share/nginx/html &>>$LOG && rm -rf * &>>$LOG
 STAT_CHECK $?
 
-print "Extract Frontend Archive"
-unzip /tmp/frontend.zip &>>LOG &&  mv frontend-main/* . &>>$LOG && mv static/* . &>>LOG &&  rm -rf frontend-master static &>>LOG
+PRINT "Extract Frontend Archive"
+unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG  && rm -rf frontend-master static &>>$LOG
 STAT_CHECK $?
 
-print " copy Roboshop config\t"
- mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>LOG
+PRINT "Copy RoboShop Config\t"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
 STAT_CHECK $?
 
-print "Enabling nginx"
-systemctl enable nginx  &>>LOG
+PRINT "Update RoboShop Config\t"
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/'  -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 STAT_CHECK $?
 
-print "Starting nginx"
-systemctl restart nginx &>>LOG
+PRINT "Enabling Nginx\t\t"
+systemctl enable nginx  &>>$LOG
 STAT_CHECK $?
 
-#surya
+PRINT "Starting Nginx\t\t"
+systemctl restart nginx  &>>$LOG
+STAT_CHECK $?
