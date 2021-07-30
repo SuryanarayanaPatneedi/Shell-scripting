c1c968d2ecc10d5041843fedd7073236779160d3
Surya <suryapatneedi0505@gmail.com>
2021-07-30 23:52:25 +0530

    asc

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -2,30 +2,35 @@
 
 source common.sh
 
-PRINT "Installing Nginx"
+PRINT "Installing Nginx\t"
 yum install nginx -y &>>$LOG
 STAT_CHECK $?
 
-PRINT "Downloading frontend"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>LOG
+PRINT "Download Frontend\t"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
 STAT_CHECK $?
-PRINT "Removing oldtx"
-cd /usr/share/nginx/html  &>>$LOG &&   rm -rf * &>>$LOG
+
+
+PRINT "Remove Old HtDocs\t"
+cd /usr/share/nginx/html &>>$LOG && rm -rf * &>>$LOG
 STAT_CHECK $?
-PRINT "Extract"
-unzip /tmp/frontend.zip  &>>$LOG &&  mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG &&  rm -rf frontend-master static &>>$LOG
-STATU_CHECK $?
-PRINT "copy roboshop config"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>LOG
+
+PRINT "Extract Frontend Archive"
+unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG  && rm -rf frontend-master static &>>$LOG
 STAT_CHECK $?
+
+PRINT "Copy RoboShop Config\t"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+STAT_CHECK $?
+
 PRINT "Update RoboShop Config\t"
 sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/'  -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/shipping/ s/localhost/shipping.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 STAT_CHECK $?
 
-PRINT "Enabling Nginx"
+PRINT "Enabling Nginx\t\t"
 systemctl enable nginx  &>>$LOG
 STAT_CHECK $?
-PRINT   "Starting  Nginx"
 
+PRINT "Starting Nginx\t\t"
 systemctl restart nginx  &>>$LOG
 STAT_CHECK $?
