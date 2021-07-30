342b2223a94a9a011969f6055077c658eb6fc0b8
Surya <suryapatneedi0505@gmail.com>
2021-07-31 00:55:08 +0530

    asc

--- terraform/roboshop-shell-scripting/ec2.tf
+++ terraform/roboshop-shell-scripting/ec2.tf
@@ -37,12 +37,12 @@ resource "null_resource" "run-shell-scripting" {
       password              = "DevOps321"
     }
 
-    inline = [
-      "cd /home/centos",
-      "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting",
-      "cd shell-scripting/roboshop",
-      "sudo make ${element(var.COMPONENTS, count.index)}"
-    ]
+    #inline = [
+      #"cd /home/centos",
+      #"git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting",
+      #"cd shell-scripting/roboshop",
+      #"sudo make ${element(var.COMPONENTS, count.index)}"
+    #]
   }
 }
 
