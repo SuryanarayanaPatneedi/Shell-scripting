8d4c46889126a263d65be94e2c1241bdedcebc80
Surya <suryapatneedi0505@gmail.com>
2021-07-30 16:19:35 +0530

    asc

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -3,7 +3,7 @@
 LOG=/tmp/roboshop.lg
 rm -f $LOG
 
-echo -e "Instaling Nginx\t\t..."
+echo -e "Instaling Nginx\t\t.."
 yum install nginx -y &>>$LOG
 if [ $? -eq 0 ] ; then
   echo done
@@ -13,6 +13,9 @@ if [ $? -eq 0 ] ; then
 
 echo -e "Enabling Nginx\t\t..\t\e[32mdone\e[0m"
 systemctl enable nginx  &>>$LOG
+echo $?
+
 
 echo -e  "Starting  Nginx\t\t..\t\e[32mdone\e[0m"
 systemctl start nginx  &>>$LOG
+echo $?
