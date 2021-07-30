44fbf9506860424a968198d89983fb6016cb6bc0
Surya <suryapatneedi0505@gmail.com>
2021-07-30 16:26:19 +0530

    asc

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -11,14 +11,14 @@ else
     echo fail
     fi
 
-echo -e "Enabling Nginx\t\t..\t\e[32mdone\e[0m"
+echo -e "Enabling Nginx\t\t.."
 systemctl enable nginx  &>>$LOG
 if [ $? -eq 0 ] ; then
   echo -e "\e[32m done\e[0m"
 else
     echo fail
     fi
-echo -e  "Starting  Nginx\t\t..\t\e[32mdone\e[0m"
+echo -e   "Starting  Nginx\t\t.."
 systemctl start nginx  &>>$LOG
 if [ $? -eq 0 ] ; then
   echo -e "\e[32m done\e[0m"
