3346cc46d30dfdd26a6407f42e9f408f73439216
Surya <suryapatneedi0505@gmail.com>
2021-07-30 17:13:16 +0530

    asc

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -2,29 +2,23 @@
 LOG=/tmp/roboshop.lg
 rm -f $LOG
 
-echo -e "Installing Nginx\t\t.."
-yum installl nginx -y &>>$LOG
-if [ $? -eq 0 ] ; then
+STATUS_CHECK(){
+  if [ $1 -eq 0 ] ; then
   echo -e "\e[32m done\e[0m"
 else
-    echo -e "\e[32m fail"
+    echo -e "\e[32m fail\e[0m"
     exit 1
     fi
+}
+
+echo -e "Installing Nginx\t\t.."
+yum install nginx -y &>>$LOG
+STATUS_CHECK $?
 
 echo -e "Enabling Nginx\t\t.."
 systemctl enable nginx  &>>$LOG
-if [ $? -eq 0 ] ; then
-  echo -e "\e[32m done\e[0m"
-else
-    echo -e "\e[32m fail\e[0m"
-    exit 1
-    fi
+STATUS_CHECK $?
 echo -e   "Starting  Nginx\t\t.."
 
 systemctl restart nginx  &>>$LOG
-if [ $? -eq 0 ] ; then
-  echo -e "\e[32m done\e[0m"
-  else
-    echo -e "\e[32m fail\e[0m"
-    exit 1
-    fi
+STATUS_CHECK $?
