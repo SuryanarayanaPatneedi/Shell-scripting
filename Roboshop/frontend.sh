3c67cfc0a7bb691b2d5c656c46eedd787043dfd2
Surya <suryapatneedi0505@gmail.com>
2021-07-30 17:21:00 +0530

    asc

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-LOG=/tmp/roboshop.lg
-rm -f $LOG
-
-STATUS_CHECK(){
-  if [ $1 -eq 0 ] ; then
-  echo -e "\e[32m done\e[0m"
-else
-    echo -e "\e[32m fail\e[0m"
-    exit 1
-    fi
-}
-PRINT(){
-  echo -n -e "$1\t\t.."
-}
 
 PRINT "Installing Nginx"
 yum install nginx -y &>>$LOG
