db2a540b0602fccaaab8215685967ee8fa957c02
Surya <suryapatneedi0505@gmail.com>
2021-07-30 19:27:42 +0530

    asc

--- Roboshop/mongodb.sh
+++ Roboshop/mongodb.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
-source common.sh
+souurce common.sh
 
-PRINT "Setting Up Repository\t"
+PRINT "Seeting up repository"
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-STAT_CHECK $?
+STATUS_CHECK  $?
+#Install Mongo & Start Service.
+# yum install -y mongodb-org
+# systemctl enable mongod
+# systemctl start mongod
+#Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
+#Config file: /etc/mongod.conf
 
-PRINT "Install MongoDB\t\t"
-yum install -y mongodb-org &>>$LOG
-STAT_CHECK $?
+#then restart the service
 
-PRINT "Update MongoDB Listen Address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-STAT_CHECK $?
+# systemctl restart mongod
+#Every Database needs the schema to be loaded for the application to work.
+#Download the schema and load it.
 
-PRINT "Start MongoDB Service\t"
-systemctl enable mongod &>>$LOG && systemctl start mongod &>>$LOG
-STAT_CHECK $?
+# curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 
-PRINT "Download MongoDB Schema\t"
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG
-STAT_CHECK $?
-
-PRINT "Load Schema\t\t"
-cd /tmp && unzip -o  mongodb.zip &>>$LOG && cd mongodb-main && mongo < catalogue.js &>>$LOG && mongo < users.js &>>$LOG
-STAT_CHECK $?
-
+# cd /tmp
+# unzip mongodb.zip
+# cd mongodb-main
+# mongo < catalogue.js
+# mongo < users.js
