7d45dc4fe02dfa13d5a294109ff78346d5b95c4f
Surya <suryapatneedi0505@gmail.com>
2021-07-30 22:11:09 +0530

    asc

--- Roboshop/redis.sh
+++ Roboshop/redis.sh
@@ -3,7 +3,6 @@
 source common.sh
 
 PRINT "Install Redis Repos\t"
-#yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
 yum install epel-release yum-utils https://ftp.igh.cnrs.fr/pub/remi/enterprise/remi-release-7.rpm  -y &>>$LOG
 STAT_CHECK $?
 
@@ -12,7 +11,7 @@ yum install redis -y --enablerepo=remi &>>$LOG
 STAT_CHECK $?
 
 PRINT "Update Redis Listen Address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
 STAT_CHECK $?
 
 PRINT "Start Redis Service\t"
