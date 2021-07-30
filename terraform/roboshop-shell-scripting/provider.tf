0ae924b28f269fd9a9dc0d902376cf2194d018f8
Surya <suryapatneedi0505@gmail.com>
2021-07-30 14:39:33 +0530

    variable

--- terraform/roboshop-shell-scripting/provider.tf
+++ terraform/roboshop-shell-scripting/provider.tf
@@ -3,11 +3,3 @@ provider "aws" {
 }
 
 
-terraform {
-  backend "s3" {
-    bucket            = "surya-terraform"
-    key               = "roboshop/shell-scripting/terraform.tfstate"
-    region            = "us-east-1"
-    dynamodb_table    = "terraform"
-  }
-}
