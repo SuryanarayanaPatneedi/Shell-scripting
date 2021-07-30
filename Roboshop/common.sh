5e726024b57b0110631271e867ae773dc0909a57
Surya <suryapatneedi0505@gmail.com>
2021-07-30 23:45:52 +0530

    asc

--- Roboshop/common.sh
+++ Roboshop/common.sh
@@ -1,27 +1,52 @@
 #!/bin/bash
 
-
 USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]; then
   echo -e "\e[31mYou should be root user / sudo user to run this script\e[0m"
   exit 2
 fi
 
-
-LOG=/tmp/roboshop.lg
+LOG=/tmp/roboshop.log
 rm -f $LOG
 
-STAT_CHECK(){
-  if [ $1 -eq 0 ] ; then
-  echo -e "\e[32m done\e[0m"
-else
-    echo -e "\e[32m fail\e[0m"
+STAT_CHECK() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[32m done\e[0m"
+  else
+    echo -e "\e[31m fail\e[0m"
+    echo -e "\e[33m Check the log file for more details, Log File - $LOG\e[0m"
     exit 1
-    fi
+  fi
 }
+
 PRINT() {
   echo -e "\n###########################\t$1\t###########################" &>>$LOG
   echo -n -e "$1\t\t..."
+}
+
+ADD_APPLICATION_USER() {
+  PRINT "Add RoboShop Application User"
+  id roboshop &>>$LOG
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>$LOG
+  fi
+  STAT_CHECK $?
+}
+
+DOWNLOAD_APP_CODE() {
+  PRINT "Download Application Code"
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG
+  STAT_CHECK $?
+
+  PRINT "Extract Downloaded Code\t"
+  cd /home/roboshop && unzip -o  /tmp/${COMPONENT}.zip &>>$LOG  && rm -rf ${COMPONENT} && mv ${COMPONENT}-main ${COMPONENT}
+  STAT_CHECK $?
+}
+
+PERM_FIX() {
+  PRINT "Fix Application Permissions"
+  chown roboshop:roboshop /home/roboshop -R &>>$LOG
+  STAT_CHECK $?
 }
 
 SETUP_SYSTEMD() {
@@ -50,3 +75,40 @@ NODEJS() {
   SETUP_SYSTEMD
 }
 
+JAVA() {
+  PRINT "Install Maven\t\t"
+  yum install maven -y &>>$LOG
+  STAT_CHECK $?
+
+  ADD_APPLICATION_USER
+  DOWNLOAD_APP_CODE
+
+  PRINT "Compile Code\t\t"
+  cd /home/roboshop/${COMPONENT} && mvn clean package &>>$LOG &&  mv target/shipping-1.0.jar shipping.jar
+  STAT_CHECK $?
+
+  PERM_FIX
+  SETUP_SYSTEMD
+}
+
+PYTHON3() {
+  PRINT "Install Python3\t\t"
+  yum install python36 gcc python3-devel -y &>>$LOG
+  STAT_CHECK $?
+
+  ADD_APPLICATION_USER
+  DOWNLOAD_APP_CODE
+
+  PRINT "Install Python Dependencies"
+  cd /home/roboshop/${COMPONENT} && pip3 install -r requirements.txt &>>$LOG
+  STAT_CHECK $?
+
+  PRINT "Update Service Configuration"
+  userID=$(id -u roboshop)
+  groupID=$(id -g roboshop)
+  sed -i -e "/uid/ c uid = ${userID}" -e "/gid/ c gid = ${groupID}" payment.ini &>>$LOG
+  STAT_CHECK $?
+
+  PERM_FIX
+  SETUP_SYSTEMD
+}
