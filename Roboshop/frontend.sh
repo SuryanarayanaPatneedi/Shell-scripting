cd28c3cc75ca8ab7ae76c1e6e2f850424da3d22c
Surya <suryapatneedi0505@gmail.com>
2021-07-30 17:02:34 +0530

    asc

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -3,7 +3,7 @@ LOG=/tmp/roboshop.lg
 rm -f $LOG
 
 echo -e "Installing Nginx\t\t.."
-yum install nginx -y &>>$LOG
+yum installl nginx -y &>>$LOG
 if [ $? -eq 0 ] ; then
   echo -e "\e[32m done\e[0m"
 else
